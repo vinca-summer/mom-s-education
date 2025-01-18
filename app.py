a6aa4b5dde86695cb2946ec9673cb651ded10c4a
vinca-summer <vinca.summer@gmail.com>
2025-01-18 22:10:52 +0100

    comment heree

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 print('hello, mom')
 input()
+<<<<<<< Updated upstream
 ### my comment here
+=======
+### my comment here and one more comment here
+>>>>>>> Stashed changes
+# my more comments better comments
